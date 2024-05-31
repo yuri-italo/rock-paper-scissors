59726d7b5db399027f522b27c16daa2480ce4024
yuri-italo <yuri.italo94@gmail.com>
2024-05-31 14:57:55 -0300

    Add playRound function for game logic
    Implement the playRound function to handle the game logic of
    rock-paper-scissors. This function determines the winner between the
    human and the computer based on their choices. It utilizes an object to
    map each choice to what it beats, simplifying the winning/losing
    conditions.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const possibleChoices = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(possibleChoices) {
   return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
@@ -13,4 +15,26 @@ function getHumanChoice(possibleChoices) {
   } while (possibleChoices.indexOf(humanChoice) === -1);
 
   return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie! Neither the computer nor the human won.");
+  } else {
+    const beats = {
+      rock: "scissors",
+      paper: "rock",
+      scissors: "paper",
+    };
+
+    const humanWins = beats[humanChoice] === computerChoice;
+
+    if (humanWins) {
+      humanScore++;
+      console.log(`You won! ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}.`);
+    } else {
+      computerScore++;
+      console.log(`You lose! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}.`);
+    }
+  }
 }
