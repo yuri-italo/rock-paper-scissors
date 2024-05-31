41b75c1830b6421efc0080998532648e9b4c07b7
yuri-italo <yuri.italo94@gmail.com>
2024-05-31 13:51:19 -0300

    Add the function getHumanChoice
    Create the function getHumanChoice that return
    the choice of the human player.
    
    This is function is needed to get the choice of
    the human player.
    
    The choice is get from the browser prompt and only
    return the value if it is a valid choice.

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function getHumanChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  let humanChoice;
+
+  do {
+    humanChoice = window.prompt(
+      "What is your choice? [rock, paper or scissors]"
+    );
+  } while (choices.indexOf(humanChoice) === -1);
+
+  return humanChoice;
+}
