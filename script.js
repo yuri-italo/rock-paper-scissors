90af59758c2cf0d4a1eed84f84920bd7c7ee1420
yuri-italo <yuri.italo94@gmail.com>
2024-05-31 15:42:01 -0300

    Add playGame function for rock-paper-scissors
    Implement the playGame function to play a specified number of rounds
    of rock-paper-scissors. The function tracks scores for both the human
    and the computer. A playRound function determines the winner of each
    round, updating the score accordingly. Helper functions are added to
    get choices for both the human and the computer.

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-const possibleChoices = ["rock", "paper", "scissors"];
-let humanScore = 0;
-let computerScore = 0;
+function playGame(roundsToPlay) {
+  const possibleChoices = ["rock", "paper", "scissors"];
+  let humanScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < roundsToPlay; i++) {
+    const humanChoice = getHumanChoice(possibleChoices);
+    const computerChoice = getComputerChoice(possibleChoices);
+    playRound(humanChoice, computerChoice);
+  }
+
+  console.log(`Human: ${humanScore} VS Computer: ${computerScore}`);
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log("It's a tie! Neither the computer nor the human won.");
+    } else {
+      const beats = {
+        rock: "scissors",
+        paper: "rock",
+        scissors: "paper",
+      };
+
+      const humanWins = beats[humanChoice] === computerChoice;
+
+      if (humanWins) {
+        humanScore++;
+        console.log(
+          `You won! ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}.`
+        );
+      } else {
+        computerScore++;
+        console.log(
+          `You lose! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}.`
+        );
+      }
+    }
+  }
+}
 
 function getComputerChoice(possibleChoices) {
   return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
@@ -17,24 +53,4 @@ function getHumanChoice(possibleChoices) {
   return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    console.log("It's a tie! Neither the computer nor the human won.");
-  } else {
-    const beats = {
-      rock: "scissors",
-      paper: "rock",
-      scissors: "paper",
-    };
-
-    const humanWins = beats[humanChoice] === computerChoice;
-
-    if (humanWins) {
-      humanScore++;
-      console.log(`You won! ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}.`);
-    } else {
-      computerScore++;
-      console.log(`You lose! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}.`);
-    }
-  }
-}
+playGame(5);
