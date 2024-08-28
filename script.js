e979310512e6bda77d5f83c84f9d0b4b95f07ec6
yuri-italo <yuri.italo94@gmail.com>
2024-08-28 18:24:24 -0300

    feat: add rps ui

--- script.js
+++ script.js
@@ -1,56 +1,62 @@
-function playGame(roundsToPlay) {
-  const possibleChoices = ["rock", "paper", "scissors"];
-  let humanScore = 0;
-  let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-  for (let i = 0; i < roundsToPlay; i++) {
-    const humanChoice = getHumanChoice(possibleChoices);
-    const computerChoice = getComputerChoice(possibleChoices);
-    playRound(humanChoice, computerChoice);
-  }
+const beats = { rock: "scissors", paper: "rock", scissors: "paper" };
 
-  console.log(`Human: ${humanScore} VS Computer: ${computerScore}`);
+const humanScoreSpan = document.querySelector(".human-score");
+const computerScoreSpan = document.querySelector(".computer-score");
+const choiceContainer = document.querySelector(".choice-container");
+const resultP = document.querySelector(".result-container p");
+const buttons = document.querySelectorAll(".choice-button");
 
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-      console.log("It's a tie! Neither the computer nor the human won.");
+buttons.forEach((button) => {
+  button.addEventListener("click", playRound);
+});
+
+function playRound(event) {
+  const computerChoice = getComputerChoice().trim().toLowerCase();
+  const humanChoice = event.target
+    .closest("button")
+    .innerText.trim()
+    .toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    resultP.innerText = "It's a tie! Neither the computer nor you won.";
+  } else {
+    const humanWins = beats[humanChoice] === computerChoice;
+
+    if (humanWins) {
+      humanScoreSpan.innerText = ++humanScore;
+      resultP.innerText = `You won! ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}.`;
     } else {
-      const beats = {
-        rock: "scissors",
-        paper: "rock",
-        scissors: "paper",
-      };
-
-      const humanWins = beats[humanChoice] === computerChoice;
-
-      if (humanWins) {
-        humanScore++;
-        console.log(
-          `You won! ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}.`
-        );
-      } else {
-        computerScore++;
-        console.log(
-          `You lose! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}.`
-        );
-      }
+      computerScoreSpan.innerText = ++computerScore;
+      resultP.innerText = `You lose! ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}.`;
     }
   }
+
+  if (humanScore >= 5 || computerScore >= 5) {
+    if (humanScore > computerScore) {
+      resultP.innerText = "🎉 CONGRATULATIONS! You’re victorious!";
+    } else {
+      resultP.innerText =
+        "😔 GAME OVER! The computer won this time. Try again for a comeback!";
+    }
+
+    buttons.forEach((button) => {
+      button.style.display = "none";
+    });
+
+    const playAgain = document.createElement("button");
+    playAgain.innerHTML = 'Play Again <i class="fa-solid fa-rotate-right"></i>';
+    choiceContainer.appendChild(playAgain);
+
+    playAgain.addEventListener("click", () => {
+      window.location.reload();
+    });
+  }
 }
 
-function getComputerChoice(possibleChoices) {
+function getComputerChoice() {
+  const possibleChoices = ["rock", "paper", "scissors"];
   return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
 }
-
-function getHumanChoice(possibleChoices) {
-  let humanChoice;
-  do {
-    humanChoice = window.prompt(
-      "What is your choice? [rock, paper or scissors]"
-    );
-  } while (possibleChoices.indexOf(humanChoice) === -1);
-
-  return humanChoice;
-}
-
-playGame(5);
